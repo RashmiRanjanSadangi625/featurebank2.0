1cf80daa93599dd74b3a17d5e4c9c2fca218ad50
Rashmi Ranjan Sadangi <72812857+RashmiRanjanSadangi625@users.noreply.github.com>
2024-08-09 03:11:15 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,15 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/featuredb', 
-	{ useNewUrlParser: true, useUnifiedTopology: true })
-	.then(()=>{console.log("Connection Succesfull...!");})
-	.catch((e)=> {console.log(e);});
+const uri = 'mongodb+srv://db_user:1234@cluster0.mongodb.net/featuredb?retryWrites=true&w=majority';
 
-
-// mongoose.connect("mongodb://localhost:27017/featuredb",
-// 	{
-// 		useNewUrlParser : true,
-// 		useUnifiedTopology:true
-// 	})
-// .then(()=>{console.log("Connection Succesfull...!");})
-// .catch((e)=> {console.log(e);})
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connection established successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
